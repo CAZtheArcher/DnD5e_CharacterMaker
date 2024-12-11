8fe955521b8c1d5e913c4af2d0dbb496e6f348df
CAZtheArcher <caz1158@g.rit.edu>
2024-12-10 19:08:07 -0500

    router.js has domo removed

--- server/router.js
+++ server/router.js
@@ -2,10 +2,8 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos); //domo line - delete or edit
   app.get('/getChars', mid.requiresLogin, controllers.Character.getCharacter);
   
-  app.post('/deleteDomo', mid.requiresLogin, controllers.Domo.deleteDomo); //domo line - delete or edit
   app.post('/deleteChar', mid.requiresLogin, controllers.Character.deleteCharacter);
 
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
