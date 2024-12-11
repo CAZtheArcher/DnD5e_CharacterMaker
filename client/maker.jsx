adbb86d5118b60ed3d108bf5d59d28e24603b0fb
CAZtheArcher <caz1158@g.rit.edu>
2024-12-10 20:17:02 -0500

    removed domo from maker.jsx

--- client/maker.jsx
+++ client/maker.jsx
@@ -193,27 +193,6 @@ const CharForm = (props) => {
     );
 };
 
-const DomoForm = (props) => {
-    return(
-        <form id="domoForm"
-            onSubmit={(e) => handleDomo(e, props.triggerReload)}
-            name="domoForm"
-            action="/maker"
-            method="POST"
-            className="domoForm"
-        >
-            <label htmlFor="name">Name: </label>
-            <input id="domoName" type="text" name="name" placeholder="Domo Name" />
-            <label htmlFor="age">Age: </label>
-            <input id="domoAge" type="number" min="0" name="age" placeholder='age' />
-
-            <label htmlFor="power">PowLVL: </label>
-            <input id="domoPower" type="number" min="0" name="power" placeholder='power level' />
-            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
-        </form>
-    );
-};
-
 const RemoveCharForm = (props) => {
     return(
         <form id="charRemoveForm"
@@ -227,60 +206,6 @@ const RemoveCharForm = (props) => {
             <input id="charName" type="text" name="name" placeholder="Character to Remove" />
             <input className="makeCharSubmit" type="submit" value="Remove Char" />
         </form>
-    );
-};
-
-const RemoveDomoForm = (props) => {
-    return(
-        <form id="domoForm"
-        onSubmit={(e) => handleRemove(e, props.triggerReload)}
-            name="domoForm"
-            action="/deleteDomo"
-            method="POST"
-            className="domoForm"
-        >
-            <label htmlFor="name">Name: </label>
-            <input id="domoName" type="text" name="name" placeholder="Domo to Remove" />
-            <input className="makeDomoSubmit" type="submit" value="Remove Domo" />
-        </form>
-    );
-};
-
-const DomoList = (props) => {
-    const [domos, setDomos] = useState(props.domos);
-
-    useEffect(() => {
-        const loadDomosFromServer = async () => {
-            const response = await fetch('/getDomos');
-            const data = await response.json();
-            setDomos(data.domos);
-        };
-        loadDomosFromServer();
-    }, [props.reloadDomos]);
-
-    if(domos.length === 0){
-        return (
-            <div className="domoList">
-                <h3 className="emptyDomo">No Domos Yet!</h3>
-            </div>
-        );
-    }
-
-    const domoNodes = domos.map(domo => {
-        return(
-            <div key={domo.id} className="domo">
-                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
-                <h3 className="domoName">Name: {domo.name}</h3>
-                <h3 className="domoAge">Age: {domo.age}</h3>
-                <h3 className="domoPower">Power Level: {domo.power}</h3>
-            </div>
-        );
-    });
-
-    return(
-        <div className="domoList">
-            {domoNodes}
-        </div>
     );
 };
 
@@ -307,7 +232,6 @@ const CharList = (props) => {
     const charNodes = chars.map(char => {
         return(
             <div key={char.id} className="char">
-                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
                 <h3 className="charName">Name: {char.name}</h3>
                 <h3 className="charLevel">Level: {char.level}</h3>
                 <h3 className="charRace">Race: {char.race}</h3>
@@ -319,24 +243,6 @@ const CharList = (props) => {
     return(
         <div className="charList">
             {charNodes}
-        </div>
-    );
-};
-
-const DomoApp = () => {
-    const [reloadDomos, setReloadDomos] = useState(false);
-
-    return(
-        <div>
-            <div id="makeDomo">
-                <DomoForm triggerReload={() => setReloadDomos(!reloadDomos)} />
-            </div>
-            <div id="makeDomo">
-                <RemoveDomoForm triggerReload={() => setReloadDomos(!reloadDomos)} />
-            </div>
-            <div id="domos">
-                <DomoList domos={[]} reloadDomos={reloadDomos} />
-            </div>
         </div>
     );
 };
